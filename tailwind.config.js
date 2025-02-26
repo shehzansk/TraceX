96fb7ed629e14813d777b8309b6198650a05d122
VidyutChakrabarti <chakrabartivr@rknec.edu>
2025-02-26 10:13:35 +0530

    Rewrote package.json with stable dependencies. Stable ver 1.0, can be used for setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
-import { nextui } from '@nextui-org/theme'
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
